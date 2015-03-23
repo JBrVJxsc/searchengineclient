f63a0a1533138879ea71592c78bb619d1c1b8484
Xu ZHANG <laonawuli@126.com>
2015-03-22 18:03:17 -0700

    Code.

--- SearchEngineClient/main.cpp
+++ SearchEngineClient/main.cpp
@@ -19,12 +19,11 @@ int main(int argc, char** argv)
     string message;
     char line[256];
     
-    // 此处用localhost与9999端口测试。
-    char ip[] = "localhost";
-    char port[] = "9999";
+    char ip[] = "localhost"; // IP地址。
+    int port = 9999; // 端口号。
     
     TCPConnector* connector = new TCPConnector();
-    TCPStream* stream = connector->connect(ip, atoi(port));
+    TCPStream* stream = connector->connect(ip, port);
     if (stream) {
         message = "Is there life on Mars?";
         stream->send(message.c_str(), message.size());
@@ -35,7 +34,7 @@ int main(int argc, char** argv)
         delete stream;
     }
     
-    stream = connector->connect(ip, atoi(port));
+    stream = connector->connect(ip, port);
     if (stream) {
         message = "Why is there air?";
         stream->send(message.c_str(), message.size());
