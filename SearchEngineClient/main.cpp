c0e1f83924fcc1631a8280a91a1f9d047503f5e0
nkwangyuan <nkwangyuan@gmail.com>
2015-03-24 21:08:06 -0700

    add update and query by transfer file name

--- SearchEngineClient/main.cpp
+++ SearchEngineClient/main.cpp
@@ -15,23 +15,47 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    char result[256]; // 视情况，可调整该缓存大小。
-    //
+    char result[512]; // buffer
     
-    char ip[] = "localhost"; // IP地址。
-    int port = 9999; // 端口号。
+    char ip[] = "localhost"; // IP address
+    int port = 9999; // port
     
     TCPConnector* connector = new TCPConnector();
     TCPStream* stream = connector->connect(ip, port);
+    
+    bool isQuery = true;
+    
     if (stream) {
-        string message = "UPDATE(or QUERY) + DATA"; // 在此处用打头字母指示即将要让服务器进行的操作，既“更新”或“查询”；后面接着被操作的DATA。
-        stream->send(message.c_str(), message.size()); // 发送数据给服务器。
-        int len = stream->receive(result, sizeof(result)); // 在此处接受服务器返回的信息，例如错误信息（更新）或结果（查询）等。
-        result[len] = NULL;
-        // 在此处处理result，例如显示错误信息或者显示结果等。
-        // Do something here.
-        printf("RESULT: %s\n", result);
-        delete stream;
+        if (isQuery)
+        {//send a query
+            string message = "QUERY";
+            string filequery = "dataset.hdf5";//the filename of query.
+            message = message +" "+filequery;
+            
+            
+            stream->send(message.c_str(), message.size()); // send data to server
+            int len = stream->receive(result, sizeof(result)); // receive the message from server. err_msg or results
+            
+            result[len] = NULL;
+            printf("Received:  %s\n", result);
+            delete stream;
+        }
+        else
+        {//send a update message
+            string message = "UPDATE";
+            string filequery = "dataset.hdf5";//the filename of query.
+            message = message +" "+filequery;
+            
+            stream->send(message.c_str(), message.size()); // send data to server
+            int len = stream->receive(result, sizeof(result)); // receive the message from server. err_msg or results
+            
+            result[len] = NULL;
+            printf("Received:  %s\n", result);
+            
+            delete stream;
+            
+        }
+        
     }
     
     exit(0);
